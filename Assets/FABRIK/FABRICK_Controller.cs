1e589ec5ab0972009c696256134a5bd6e6c9ff58
QuestGiver <cpt.gastard@gmail.com>
2018-11-28 12:46:25 -0800

    add worki in FABRIK_Controller

--- Assets/FABRIK/FABRICK_Controller.cs
+++ Assets/FABRIK/FABRICK_Controller.cs
@@ -50,17 +50,17 @@ public class FABRICK_Controller : MonoBehaviour
 
             //find the distance (joint target distance [i]) between targetPosition and the current joint
             float[] JointTargetDistance = new float[jointPosition.Length];//r
-            float[] JointSepOverTargDist = new float[jointPosition.Length];//lambda
+            float[] jointSepOverTargDist = new float[jointPosition.Length];//lambda
 
             for (int i = 0; i < jointPosition.Length; i++)
             {
                 JointTargetDistance[i] = Vector3.Distance(jointPosition[i].position, targetPosition.position);
-                JointSepOverTargDist[i] = jointSeperationDistances[i] / JointTargetDistance[i];
+                jointSepOverTargDist[i] = jointSeperationDistances[i] / JointTargetDistance[i];
             }
             //find new join position
             for (int i = 0; i < jointPosition.Length; i++)
             {
-                jointPosition[i + 1].position = (1 - JointSepOverTargDist[i]) * jointPosition[i].position + JointSepOverTargDist[i] * targetPosition.position;
+                jointPosition[i + 1].position = (1 - jointSepOverTargDist[i]) * jointPosition[i].position + jointSepOverTargDist[i] * targetPosition.position;
             }
         }
         else
@@ -81,7 +81,7 @@ public class FABRICK_Controller : MonoBehaviour
 
 
                 float[] jointEndEffectorDistance = new float[jointPosition.Length];//r
-                float[] JointSepOverTargDist = new float[jointPosition.Length];//lambda
+                float[] jointSepOverTargDist = new float[jointPosition.Length];//lambda
                 for (int i = 0; i < jointPosition.Length; i++)
                 {
                     //find the distance (Ri) between the new joint position (Pi+1) and the joint (Pi)
@@ -90,14 +90,33 @@ public class FABRICK_Controller : MonoBehaviour
 
                 for (int i = 0; i < jointPosition.Length; i++)
                 {
-                    JointSepOverTargDist[i] = jointSeperationDistances[i] / jointEndEffectorDistance[i];
+                    jointSepOverTargDist[i] = jointSeperationDistances[i] / jointEndEffectorDistance[i];
                 }
 
-                //find the new joint positions
+                //find the new joint positions P[i]
+
+                for (int i = 0; i < jointPosition.Length; i++)
+                {
+                    jointPosition[i].position = ((1 - jointSepOverTargDist[i]) * jointPosition[i + 1].position) + jointSepOverTargDist[i] * jointPosition[i].position;
+                }
+
+            }
+            //Stage 2: Backwards reaching
+            //set the root p[0] back to its initial value
+            jointPosition[0] = rootB;
+            float[] newJointPosDistance = new float[jointPosition.Length - 1];//r[i]
+            float[] newjointSepOverTargDist = new float[jointPosition.Length];//lambda
+            for (int i = 0; i < jointPosition.Length; i++)
+            {
+                //find the distance (r[i]) between the new joint position (p[i])
+                newJointPosDistance[i] = Vector3.Distance(jointPosition[i + 1].position, jointPosition[i].position);
+                newjointSepOverTargDist[i] = jointSeperationDistances[i] / newJointPosDistance[i];
+                //find new joint positions p[i]
+                jointPosition[i].position = ((1 - newjointSepOverTargDist[i]) * jointPosition[i].position) + newjointSepOverTargDist[i] * jointPosition[i+1].position;
             }
 
-
-
+            // Check whether the distance between the end effector p[n] and the target t is greater than a tolerance.
+            reach = jointPosition[jointPosition.Length]
         }
 
 
