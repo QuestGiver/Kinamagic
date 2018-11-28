38f7113849259c41e96e64f6bd256c2a56cdead8
QuestGiver <cpt.gastard@gmail.com>
2018-11-27 17:58:19 -0800

    add FABRIK progress

--- Assets/FABRIK/FABRICK_Controller.cs
+++ Assets/FABRIK/FABRICK_Controller.cs
@@ -22,4 +22,88 @@ public class FABRICK_Controller : MonoBehaviour
     {
 
     }
+
+    public void FABRIK(Transform[] jointPosition, Transform targetPosition, float tolerance)//joint positions; p
+    {
+        float[] jointSeperationDistances = new float[jointPosition.Length - 1];//d
+
+        //distance between root and target
+        float rootDist = Vector3.Distance(jointPosition[0].position, targetPosition.position);
+
+        for (int i = 0; i < jointPosition.Length - 1; i++)
+        {
+
+            jointSeperationDistances[i] = Vector3.Distance(jointPosition[i + 1].position, jointPosition[i].position);
+
+        }
+
+        //check if target is within reach
+        float reach = 0;//assuming the link chain starts at "zero position"
+        for (int i = 0; i < jointSeperationDistances.Length; i++)
+        {
+            reach += jointSeperationDistances[i];
+        }
+        if (rootDist > reach)
+        {
+
+            //target is not within reach
+
+            //find the distance (joint target distance [i]) between targetPosition and the current joint
+            float[] JointTargetDistance = new float[jointPosition.Length];//r
+            float[] JointSepOverTargDist = new float[jointPosition.Length];//lambda
+
+            for (int i = 0; i < jointPosition.Length; i++)
+            {
+                JointTargetDistance[i] = Vector3.Distance(jointPosition[i].position, targetPosition.position);
+                JointSepOverTargDist[i] = jointSeperationDistances[i] / JointTargetDistance[i];
+            }
+            //find new join position
+            for (int i = 0; i < jointPosition.Length; i++)
+            {
+                jointPosition[i + 1].position = (1 - JointSepOverTargDist[i]) * jointPosition[i].position + JointSepOverTargDist[i] * targetPosition.position;
+            }
+        }
+        else
+        {
+            //target is within reach
+            Transform rootB = jointPosition[0];//b
+
+            Transform endEffector = jointPosition[jointPosition.Length];
+
+            while (tolerance > Vector3.Distance(jointPosition[jointPosition.Length].position, targetPosition.position))
+            {
+                //stage 1: forwards reaching
+
+                //set end effector (jointPosition[jointPosition.Length].transform) as target
+                //Pn = t
+
+                jointPosition[jointPosition.Length] = targetPosition;
+
+
+                float[] jointEndEffectorDistance = new float[jointPosition.Length];//r
+                float[] JointSepOverTargDist = new float[jointPosition.Length];//lambda
+                for (int i = 0; i < jointPosition.Length; i++)
+                {
+                    //find the distance (Ri) between the new joint position (Pi+1) and the joint (Pi)
+                    jointEndEffectorDistance[i] = Vector3.Distance(jointPosition[i + 1].position, jointPosition[i].position);
+                }
+
+                for (int i = 0; i < jointPosition.Length; i++)
+                {
+                    JointSepOverTargDist[i] = jointSeperationDistances[i] / jointEndEffectorDistance[i];
+                }
+
+                //find the new joint positions
+            }
+
+
+
+        }
+
+
+
+
+    }
+
+
 }
--- Assets/FABRIK/FABRICK_Library.cs
+++ Assets/FABRIK/FABRICK_Library.cs
@@ -5,50 +5,76 @@ using UnityEngine;
 public static class FABRICK_Library
 {
 
-
-
-    static void FABRICK()
-    {
-        //while (abs(endEffectorPosition — goalPosition) > EPS)
-        //{
-        //    FinalToRoot(); // PartOne
-        //    RootToFinal(); // PartTwo
-        //}
-    }
-
-
-    static void FinalToRoot(Transform goal, FABRIK_Joint[] Limbs)
-    {
-        FABRIK_Joint currentLimb;
-        Transform targetposition = goal;
-        currentLimb = Limbs[Limbs.Length];//(last limb in array)
-        currentLimb.incommingRotation = currentLimb.transform.position;
-
-
-        while (currentLimb != null)
-        {
-            currentLimb.transform.Rotate(currentLimb.axisOfRotation,Vector3.Angle(currentLimb.transform.rotation.eulerAngles, targetposition.position - currentLimb.incommingRotation));
-            currentLimb.outgoingRotation = target position;
-            target position = currentLimb.incommingRotation;
-            currentLimb = next limb furthur within the treebranch;
-        }
-    }
+    
 
 
 
-    static void RootToFinal(Transform goal, FABRIK_Joint[] Limbs)
-    {
-        //currentIncommingPosition = rootLimb.incommingPosition;(first limb in array)
-        //currentLimb = rootLimb;
 
 
-        //while (currentLimb != NULL)
-        //{
-        //    currentLimb.incommingPosition = currentincommingPosition;
-        //    currentincommingPosition = currentLimb.outGoingPosition;
-        //    currentLimb = next limb furthur away the treebranch root;
-        //}
-    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //static void FABRICK()
+    //{
+    //    //while (abs(endEffectorPosition — goalPosition) > EPS)
+    //    //{
+    //    //    FinalToRoot(); // PartOne
+    //    //    RootToFinal(); // PartTwo
+    //    //}
+    //}
+
+
+    //static void FinalToRoot(Transform goal, FABRIK_Joint[] Limbs)
+    //{
+    //    FABRIK_Joint currentLimb;
+    //    Transform targetposition = goal;
+    //    currentLimb = Limbs[Limbs.Length];//(last limb in array)
+    //    currentLimb.incommingRotation = currentLimb.transform.position;
+
+
+    //    while (currentLimb != null)
+    //    {
+    //        currentLimb.transform.Rotate(currentLimb.axisOfRotation,Vector3.Angle(currentLimb.transform.rotation.eulerAngles, targetposition.position - currentLimb.incommingRotation));
+    //        currentLimb.outgoingRotation = target position;
+    //        target position = currentLimb.incommingRotation;
+    //        currentLimb = next limb furthur within the treebranch;
+    //    }
+    //}
+
+
+
+    //static void RootToFinal(Transform goal, FABRIK_Joint[] Limbs)
+    //{
+    //    //currentIncommingPosition = rootLimb.incommingPosition;(first limb in array)
+    //    //currentLimb = rootLimb;
+
+
+    //    //while (currentLimb != NULL)
+    //    //{
+    //    //    currentLimb.incommingPosition = currentincommingPosition;
+    //    //    currentincommingPosition = currentLimb.outGoingPosition;
+    //    //    currentLimb = next limb furthur away the treebranch root;
+    //    //}
+    //}
 
 
 
